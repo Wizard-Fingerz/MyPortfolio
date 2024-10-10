80aa13ba6e7a5f618cec44fd51971d4883dcd756
WizCode <adewale.oladiti28@gmail.com>
2024-10-10 12:01:40 +0100

    updated

--- components/Skills/index.js
+++ components/Skills/index.js
@@ -6,15 +6,23 @@ function Skills() {
           <div className={styles.skillContainer}>
           <h2 className={styles.h2}>Skills</h2>
           <div className={styles.skillBar}>
-              <h3>Data Skills</h3>
+              <h3>Django</h3>
               <div className={styles.progressBar} style={{ width: '80%' }}></div>
             </div>
             <div className={styles.skillBar}>
-              <h3>Development Skills</h3>
+              <h3>Python for Machine Learning</h3>
               <div className={styles.progressBar} style={{ width: '100%' }}></div>
             </div>
             <div className={styles.skillBar}>
-              <h3>Design Skills</h3>
+              <h3>JavaScript</h3>
+              <div className={styles.progressBar} style={{ width: '100%' }}></div>
+            </div>
+            <div className={styles.skillBar}>
+              <h3>React Next</h3>
+              <div className={styles.progressBar} style={{ width: '100%' }}></div>
+            </div>
+            <div className={styles.skillBar}>
+              <h3>Figma</h3>
               <div className={styles.progressBar} style={{ width: '70%' }}></div>
             </div>
           </div>
