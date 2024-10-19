c82526ac3c8f17adcf33f82531a0f4df956f1875
WizCode <adewale.oladiti28@gmail.com>
2024-10-19 16:31:49 +0100

    added resume

--- components/Jumpo/index.js
+++ components/Jumpo/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import styles from './Jumpo.module.css';
+import Resume from '../../public/assets/Adewale Oladiti John Resume 2024.pdf'
 
 const JumpoBody = () => {
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = Resume;
+        link.download = 'Adewale Oladiti John Resume 2024.pdf';
+        link.click();
+    };
+
+
     return <section className={styles.jump}>
         <div className={styles.heroContent}>
             <h2>Adewale John Oladiti - Full Stack Developer</h2>
             <p>Crafting innovative solutions in the world of web and mobile development.</p>
-            <button className={styles.downloadButton}>Download CV</button>
-        </div>
+            <button className={styles.downloadButton} onClick={handleDownload}>Download CV</button>
+            </div>
         <div className={styles.heroImage}></div>
 
     </section>;
