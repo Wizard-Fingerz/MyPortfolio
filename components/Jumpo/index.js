a30d5f7166ff0c564ede16e2da8f41d04da9101a
WizCode <adewale.oladiti28@gmail.com>
2025-01-11 15:58:14 +0100

    updated

--- components/Jumpo/index.js
+++ components/Jumpo/index.js
@@ -5,17 +5,28 @@ const JumpoBody = () => {
     const handleDownload = () => {
         const link = document.createElement('a');
         link.href = '/assets/Adewale Oladiti John Resume 2024.pdf'; // Use the relative path
-        link.download = 'Adewale Oladiti John Resume 2024.pdf';
+        link.download = 'Adewale Oladiti John Resume 2025.pdf';
         link.click();
     };
 
-
+    const handleDownload2 = () => {
+        const link = document.createElement('a');
+        link.href = '/assets/Oladiti Adewale John CV.pdf'; // Use the relative path
+        link.download = 'Adewale Oladiti John Resume 2025.pdf';
+        link.click();
+    };
+    
 
     return <section className={styles.jump}>
         <div className={styles.heroContent}>
             <h2>Adewale John Oladiti - Full Stack Developer</h2>
             <p>Crafting innovative solutions in the world of web and mobile development.</p>
-            <button className={styles.downloadButton} onClick={handleDownload}>Download CV</button>
+
+            <div>
+
+            <button className={styles.downloadButton} onClick={handleDownload}>Download Full Stack CV</button>
+            <button className={styles.downloadButton} onClick={handleDownload2}>Download Python Dev CV</button>
+            </div>
             </div>
         <div className={styles.heroImage}></div>
 
